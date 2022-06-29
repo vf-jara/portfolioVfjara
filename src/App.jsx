f2d57fbeb23bed06b1ac0e7a38fadf23e4e35525
Vinicius Jara <viniciusfjara@gmail.com>
2022-06-29 15:44:45 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <div>
+      <h1 className="text-2xl font-bold">Hello World</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <div className="w-full bg-gray-900 p-6 text-white border-b border-gray-700">
+            <div className="container flex items-center justify-between">
+                <div>Logo</div>
+                <div>Menu</div>
+            </div>
+        </div>
+    )
+}
